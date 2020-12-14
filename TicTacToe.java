0efc2b3b62b957a4d6c29c8728b9bf06ea59adf2
ketankotian <krkkotian@gmail.ocm>
2020-12-14 15:46:46 +0530

    UC 1

---
+++ TicTacToe.java
@@ -0,0 +1,19 @@
+package main;
+
+public class TicTacToe {
+
+	public static void main(String[] args) {
+
+		char board[];
+
+	}
+
+	static void layout(char board[]) {
+		board = new char[10];
+		for (int i = 0; i < board.length; i++) {
+			System.out.println(" ");
+		}
+
+	}
+
+}
